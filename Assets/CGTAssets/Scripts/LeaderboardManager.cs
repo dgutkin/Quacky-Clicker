8e1d304a683d9718558eccfbeec6ea0b3f5bb559
dgutkin <dngutkin@gmail.com>
2020-05-10 22:56:07 -0400

    enabled leaderboard for highest quack score through GameCenter on iOS

---
+++ Assets/CGTAssets/Scripts/LeaderboardManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class LeaderboardManager : MonoBehaviour
+{
+
+	ILeaderboard leaderboard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!Social.localUser.authenticated)
+            Social.localUser.Authenticate (success => {
+                //do something on success if you like
+            });
+
+		leaderboard = Social.CreateLeaderboard();
+        leaderboard.id = "HighQuacks";
+        leaderboard.LoadScores(result =>
+        {
+            Debug.Log("Received " + leaderboard.scores.Length + " scores");
+            foreach (IScore score in leaderboard.scores)
+                Debug.Log(score);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void ShowLeaderboard ()
+    {
+        if (Social.localUser.authenticated) {
+            Social.ShowLeaderboardUI ();
+            // get the quackScore from the CGTCookieManager and report to the leaderboard
+            GameObject duckManager = GameObject.Find("DuckManager");
+			CGTCookieManager duckManagerScript = duckManager.GetComponent<CGTCookieManager>();
+			ReportScore((long) duckManagerScript.quackScore, leaderboard.id);
+        }
+    }
+
+    void ReportScore(long score, string leaderboardID)
+    {
+        Debug.Log("Reporting score " + score + " on leaderboard " + leaderboardID);
+        Social.ReportScore(score, leaderboardID, success => {
+            Debug.Log(success ? "Reported score successfully" : "Failed to report score");
+        });
+    }
+
+}
