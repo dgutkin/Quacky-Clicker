a4f4ff6ca9727c4ea3f0bc2691dd3b01903cdcd8
dgutkin <dngutkin@gmail.com>
2020-05-31 14:10:26 -0400

    modified the item price formula to discount weaker items as the player power grows

--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -123,7 +123,7 @@ public class ItemManager : MonoBehaviour
 
 	public ulong CalculateItemPrice(ulong baseQps, int clickValue, int itemQps)
 	{
-		ulong itemPrice =  (baseQps + (ulong) clickValue  + (ulong) itemQps) * 60;
+		ulong itemPrice =  (ulong) ((float) ((baseQps + (ulong) clickValue  + (ulong) itemQps) * 60) / Mathf.Max(1.0f, Mathf.Log10(baseQps / (ulong) itemQps)));
 	
 		return number.RoundLargeNumber(itemPrice);
 	}
